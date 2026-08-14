3a74fcdaddbc5c2764d543b41edc73cfdb8d5e20
Gurwinder Singh <gurwindersi@adobe.com>
2026-08-14 16:34:54 +0530

    prefix internal symbol names

--- src/symbol.rs
+++ src/symbol.rs
@@ -27,22 +27,24 @@ pub struct InternalSymbolName;
 
 #[rustfmt::skip]
 impl InternalSymbolName {
-	pub const CALL                     : &[u8] = b"call";                    // Call signatures
-	pub const CONSTRUCTOR              : &[u8] = b"constructor";             // Constructor implementations
-	pub const NEW                      : &[u8] = b"new";                     // Constructor signatures
-	pub const INDEX                    : &[u8] = b"index";                   // Index signatures
-	pub const EXPORT_STAR              : &[u8] = b"export";                  // Module export * declarations
-	pub const GLOBAL                   : &[u8] = b"global";                  // Global self-reference
-	pub const MISSING                  : &[u8] = b"missing";                 // Indicates missing symbol
-	pub const TYPE                     : &[u8] = b"type";                    // Anonymous type literal symbol
-	pub const OBJECT                   : &[u8] = b"object";                  // Anonymous object literal declaration
-	pub const JSXATTRIBUTES            : &[u8] = b"jsxAttributes";           // Anonymous JSX attributes object literal declaration
-	pub const CLASS                    : &[u8] = b"class";                   // Unnamed class expression
-	pub const FUNCTION                 : &[u8] = b"function";                // Unnamed function expression
-	pub const COMPUTED                 : &[u8] = b"computed";                // Computed property name declaration with dynamic name
-	pub const ASSIGNMENT_DECLARATION   : &[u8] = b"assignment";              // Assignment declarations
-	pub const INSTANTIATION_EXPRESSION : &[u8] = b"instantiationExpression"; // Instantiation expressions
-	pub const IMPORT_ATTRIBUTES        : &[u8] = b"importAttributes";
+	// Prefix \xFE: Invalid UTF8 sequence, will never occur as IdentifierName
+	pub const CALL                     : &[u8] = b"\xFEcall";                    // Call signatures
+	pub const CONSTRUCTOR              : &[u8] = b"\xFEconstructor";             // Constructor implementations
+	pub const NEW                      : &[u8] = b"\xFEnew";                     // Constructor signatures
+	pub const INDEX                    : &[u8] = b"\xFEindex";                   // Index signatures
+	pub const EXPORT_STAR              : &[u8] = b"\xFEexport";                  // Module export * declarations
+	pub const GLOBAL                   : &[u8] = b"\xFEglobal";                  // Global self-reference
+	pub const MISSING                  : &[u8] = b"\xFEmissing";                 // Indicates missing symbol
+	pub const TYPE                     : &[u8] = b"\xFEtype";                    // Anonymous type literal symbol
+	pub const OBJECT                   : &[u8] = b"\xFEobject";                  // Anonymous object literal declaration
+	pub const JSXATTRIBUTES            : &[u8] = b"\xFEjsxAttributes";           // Anonymous JSX attributes object literal declaration
+	pub const CLASS                    : &[u8] = b"\xFEclass";                   // Unnamed class expression
+	pub const FUNCTION                 : &[u8] = b"\xFEfunction";                // Unnamed function expression
+	pub const COMPUTED                 : &[u8] = b"\xFEcomputed";                // Computed property name declaration with dynamic name
+	pub const ASSIGNMENT_DECLARATION   : &[u8] = b"\xFEassignment";              // Assignment declarations
+	pub const INSTANTIATION_EXPRESSION : &[u8] = b"\xFEinstantiationExpression"; // Instantiation expressions
+	pub const IMPORT_ATTRIBUTES        : &[u8] = b"\xFEimportAttributes";
+
 	pub const EXPORT_EQUALS            : &[u8] = b"export=";                 // Export assignment symbol
 	pub const DEFAULT                  : &[u8] = b"default";                 // Default export symbol (technically not wholly internal, but included here for usability)
 	pub const THIS                     : &[u8] = b"this";
